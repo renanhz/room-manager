2b3e5bf97f191701572b38fefdb5f7e77fe4d4b4
Renan Santos <renan.stos077@gmail.com>
2021-09-06 11:27:38 -0300

    initial commit with model

---
+++ src/main/java/com/one/digitalinnovation/roomManager/model/Room.java
@@ -0,0 +1,39 @@
+package com.one.digitalinnovation.roomManager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "meetingroom")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "date", nullable = false)
+    private String date;
+
+    @Column(name = "startHour", nullable = false)
+    private String startHour;
+
+    @Column(name = "endHour", nullable = false)
+    private String endHour;
+
+}
