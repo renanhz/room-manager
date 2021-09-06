02694132711602641728a507df41b01b822abc29
Renan Santos <renan.stos077@gmail.com>
2021-09-06 12:21:26 -0300

    get all rooms

--- src/main/java/com/one/digitalinnovation/roomManager/controller/RoomController.java
+++ src/main/java/com/one/digitalinnovation/roomManager/controller/RoomController.java
@@ -1,9 +1,26 @@
 package com.one.digitalinnovation.roomManager.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.one.digitalinnovation.roomManager.exception.ResourceNotFoundException;
+import com.one.digitalinnovation.roomManager.model.Room;
+import com.one.digitalinnovation.roomManager.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/api/v1")
 public class RoomController {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @GetMapping("/rooms")
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
+    }
+
+
 }
